5a557ffdb0d0e67b406ec29fc9a770aeab0c899c
raych2 <rtongco@gmail.com>
2021-05-01 15:48:37 -0700

    Refactor General component using hooks

--- src/components/General.js
+++ src/components/General.js
@@ -1,62 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-class General extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      phone: "",
-    };
-  }
+const General = (props) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
-  handleInputChange = (e) => {
-    const input = e.target.id;
-    this.setState({
-      [input]: e.target.value,
-    });
-  };
+  const submitted = props.submitted;
 
-  render() {
-    const submitted = this.props.submitted;
-    return submitted === false ? (
-      <div className="General form">
-        <h2>Personal Information</h2>
-        <form>
-          <label htmlFor="name">Full Name:</label>
-          <input
-            value={this.state.name}
-            type="text"
-            id="name"
-            onChange={this.handleInputChange}
-            placeholder="Curriculum Vitae"
-          />
-          <label htmlFor="email">Email:</label>
-          <input
-            value={this.state.email}
-            type="email"
-            id="email"
-            onChange={this.handleInputChange}
-            placeholder="cv@email.com"
-          />
-          <label htmlFor="phone">Phone Number:</label>
-          <input
-            value={this.state.phone}
-            type="tel"
-            id="phone"
-            onChange={this.handleInputChange}
-            placeholder="123-456-7890"
-          />
-        </form>
-      </div>
-    ) : (
-      <div className="general-view">
-        <div className="full-name">{this.state.name}</div>
-        <div className="other-personal">{this.state.email}</div>
-        <div className="other-personal">{this.state.phone}</div>
-      </div>
-    );
-  }
-}
+  return submitted === false ? (
+    <div className="General form">
+      <h2>Personal Information</h2>
+      <form>
+        <label htmlFor="name">Full Name:</label>
+        <input
+          value={name}
+          type="text"
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Curriculum Vitae"
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          value={email}
+          type="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="cv@email.com"
+        />
+        <label htmlFor="phone">Phone Number:</label>
+        <input
+          value={phone}
+          type="tel"
+          id="phone"
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="123-456-7890"
+        />
+      </form>
+    </div>
+  ) : (
+    <div className="general-view">
+      <div className="full-name">{name}</div>
+      <div className="other-personal">{email}</div>
+      <div className="other-personal">{phone}</div>
+    </div>
+  );
+};
 
 export default General;
