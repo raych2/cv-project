65df7f2e9541b1f6b19c6bda29edbde33dbd66ce
raych2 <rtongco@gmail.com>
2021-05-01 18:55:31 -0700

    Refactor EducationForm component using hooks

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -1,78 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Education from "./Education";
 
-class EducationForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      school: "",
-      degree: "",
-      start: "",
-      end: "",
-    };
-  }
+const EducationForm = (props) => {
+  const [school, setSchool] = useState("");
+  const [degree, setDegree] = useState("");
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
 
-  handleInputChange = (e) => {
-    const input = e.target.id;
-    this.setState({
-      [input]: e.target.value,
-    });
-  };
-
-  render() {
-    const { school, degree, start, end } = this.state;
-    const submitted = this.props.submitted;
-    const addSchool = this.props.addSchool;
-    const cancelSchool = this.props.cancelSchool;
-    return submitted === false ? (
-      <div className="Education form">
-        <form onSubmit={this.onSubmitEntry}>
-          <label htmlFor="school">University:</label>
-          <input
-            value={this.state.school}
-            type="text"
-            id="school"
-            onChange={this.handleInputChange}
-            placeholder="UC San Diego"
-          />
-          <label htmlFor="degree">Degree:</label>
-          <input
-            value={this.state.degree}
-            type="text"
-            id="degree"
-            onChange={this.handleInputChange}
-            placeholder="BSc Biochemistry"
-          />
-          <label htmlFor="start">Start Date:</label>
-          <input
-            value={this.state.start}
-            type="text"
-            id="start"
-            onChange={this.handleInputChange}
-            placeholder="09/2015"
-          />
-          <label htmlFor="end">End Date:</label>
-          <input
-            value={this.state.end}
-            type="text"
-            id="end"
-            onChange={this.handleInputChange}
-            placeholder="06/2019"
-          />
-        </form>
-        <div className="btn-container">
-          <button onClick={addSchool}>
-            <i className="fas fa-plus fa-lg"></i> School
-          </button>
-          <button onClick={cancelSchool}>
-            <i className="far fa-trash-alt fa-lg"></i> Form
-          </button>
-        </div>
+  const submitted = props.submitted;
+  const addSchool = props.addSchool;
+  const cancelSchool = props.cancelSchool;
+  return submitted === false ? (
+    <div className="Education form">
+      <form onSubmit={this.onSubmitEntry}>
+        <label htmlFor="school">University:</label>
+        <input
+          value={school}
+          type="text"
+          id="school"
+          onChange={(e) => setSchool(e.target.value)}
+          placeholder="UC San Diego"
+        />
+        <label htmlFor="degree">Degree:</label>
+        <input
+          value={degree}
+          type="text"
+          id="degree"
+          onChange={(e) => setDegree(e.target.value)}
+          placeholder="BSc Biochemistry"
+        />
+        <label htmlFor="start">Start Date:</label>
+        <input
+          value={start}
+          type="text"
+          id="start"
+          onChange={(e) => setStart(e.target.value)}
+          placeholder="09/2015"
+        />
+        <label htmlFor="end">End Date:</label>
+        <input
+          value={end}
+          type="text"
+          id="end"
+          onChange={(e) => setEnd(e.target.value)}
+          placeholder="06/2019"
+        />
+      </form>
+      <div className="btn-container">
+        <button onClick={addSchool}>
+          <i className="fas fa-plus fa-lg"></i> School
+        </button>
+        <button onClick={cancelSchool}>
+          <i className="far fa-trash-alt fa-lg"></i> Form
+        </button>
       </div>
-    ) : (
-      <Education school={school} degree={degree} start={start} end={end} />
-    );
-  }
-}
+    </div>
+  ) : (
+    <Education school={school} degree={degree} start={start} end={end} />
+  );
+};
 
 export default EducationForm;
